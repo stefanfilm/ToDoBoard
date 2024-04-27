1f8fb4db734c2c9129ada41d2821a3a83fad2aac
Stefan Wanigatunga <stefan.wanigatunga@gmail.com>
2024-04-27 10:52:20 -0700

    Change JavaScript Line 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
